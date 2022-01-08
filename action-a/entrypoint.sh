14c99fe9353686e918114168a9fec0bc7567ee17
rofikHa21 <69884724+rofikHa21@users.noreply.github.com>
2022-01-08 16:00:15 +0700

    Update entrypoint.sh

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,1 +1,2 @@
-sh -c "echo Hello world my name is Rofik Hakiki"
+sh -c "echo Hello world my name is $INPUT_MY_NAME"
+# $INPUT_MY_NAME adalaj variable yang aakan di isi melalui workflow file yang bernama main.yml
